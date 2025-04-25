edecb6929db805cc35fa3877d2571ed010ca740a
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2025-04-25 22:43:50 +0300

    html template files

--- HealthInformationSystem/urls.py
+++ HealthInformationSystem/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from health.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', About.name='about')
+    path('about/', About,name='about'),
 ]
