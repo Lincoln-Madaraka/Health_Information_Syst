9572ee4fb2924542b19ac01797c4f3ebfc14a42e
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2025-04-26 14:39:21 +0300

    Vercel test

--- HealthInformationSystem/settings.py
+++ HealthInformationSystem/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-e1%co3f^uaord(j(^(k3e-76svin@t%dk3694&d!+8^3ffb60i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
